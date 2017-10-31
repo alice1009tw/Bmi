2a1dbccf5d0baea41f9355c487abc79a74e8b224
alice1009tw <alice1009tw@gmail.com>
2017-10-31 12:20:50 +0800

    AlertDialog

--- app/src/main/java/com/alice/bmi/MainActivity.java
+++ app/src/main/java/com/alice/bmi/MainActivity.java
@@ -1,10 +1,12 @@
 package com.alice.bmi;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +26,15 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(edheight.getText().toString());
         float bmi = weight / (height * height);
         Log.d("MainActivity", "Your BMI is:" + bmi);
+        new AlertDialog.Builder(this)
+                .setMessage("Your BMI is " + bmi)
+                .setTitle("my title")
+                .setPositiveButton("OK", null)
+                .show();
+
+       /*Toast.makeText(this,
+                "Your BMI is " + bmi,
+                Toast.LENGTH_SHORT)   //回傳Toast
+                .show();*/
     }
 }
