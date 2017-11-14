6971eaa9ba08013cc04d6fa44eb6282c5e6d15f3
alice1009tw <alice1009tw@gmail.com>
2017-11-14 09:27:08 +0800

    Advice

--- app/src/main/java/com/alice/bmi/MainActivity.java
+++ app/src/main/java/com/alice/bmi/MainActivity.java
@@ -40,11 +40,13 @@ public class MainActivity extends AppCompatActivity {
         float height = Float.parseFloat(edheight.getText().toString());
         float bmi = weight / (height * height);
         Log.d("MainActivity", "Your BMI is:" + bmi);
-        new AlertDialog.Builder(this)
-                .setMessage(getString(R.string.your_bmi_is)+bmi)  //多國語言 抽取資料 對話框 button做事
-                .setTitle(R.string.bmi_title)
-                .setPositiveButton(R.string.ok, null)
-                .show();
+        if(bmi<20){
+            new AlertDialog.Builder(this)
+                    .setMessage(getString(R.string.your_bmi_is)+bmi+" 請多吃點")
+                    .setTitle(R.string.bmi_title)
+                    .setPositiveButton(R.string.ok, null)
+                    .show();
+        }
 
        /*Toast.makeText(this,
                 "Your BMI is " + bmi,
